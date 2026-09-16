cdbe38b398356c9c8c22f87f327d93f43d034aca
karim-software-eng <kareemmohammad909@gmail.com>
2026-09-16 04:58:43 +0300

    Add Day 2 programs

---
+++ Big_progs/prog2.py
@@ -0,0 +1,30 @@
+num1=float(input("enter first number "))
+num2=float(input("enter secand number "))
+num3=float(input("enter third number "))
+if num1==num2==num3:
+    print("all numbers are equal")
+elif num1>=num2:
+    print(num1)
+elif num2>=num3:
+    print(num2)
+else:
+    print(num3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Big_progs/prog3.py
@@ -0,0 +1,15 @@
+num=int(input("enter the num "))
+for x in range (1,num+1):
+    x+=1
+    print(x)
+
+
+
+
+
+
+
+
+
+
+
